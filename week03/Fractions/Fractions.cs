4704e75268536b7a0f92c935e7e5b7b4a9563775
hvelarderiquelme <hvelardeab@gmail.com>
2025-03-17 13:10:29 -0600

    week 3 learning activity complete

---
+++ week03/Fractions/Fractions.cs
@@ -0,0 +1,60 @@
+//Class Fractions
+public class Fractions
+{
+    private string _numerator;
+    private string _denominator;
+    //Contructor with no parameters, default 1/1
+    public Fractions()
+    {
+        _numerator = "1";
+        _denominator = "1";
+        
+    }
+    //Constructor with no denpminator, example _numerator:5, it will create 5/1 
+    public Fractions(string numerator)
+    {
+        _numerator = numerator;
+        _denominator = "1";
+        
+    }
+    //Constructor with numerator and denominator entered, example: numerator:5, denominator:4, will create 5/4
+    public Fractions(string numerator, string denominator)
+    {
+        _numerator = numerator;
+        _denominator = denominator;
+        
+    }
+
+    //Setters
+    public void SetNumerator(string numerator)
+    {
+        _numerator = numerator;
+    }
+    public void SetDenominator(string denominator)
+    {
+        _denominator = denominator;
+    }
+
+    //Getters
+    public string GetNumerator()
+    {
+        return _numerator;
+    }
+
+    public string GetDenominator()
+    {
+        return _denominator;
+    }
+
+    public string GetFractionString()
+    {
+        string _fractionString = GetNumerator() + "/" + GetDenominator();
+        return _fractionString;
+    }
+
+    public double GetDecimalValue()
+    {
+        double _decimalValue = double.Parse(GetNumerator()) / double.Parse(GetDenominator());
+        return _decimalValue;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fractions fraction1 = new Fractions();
+        Fractions fraction2 = new Fractions("6");
+        Console.WriteLine($"The fraction form for object fraction2 is: {fraction2.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction2 is: {fraction2.GetDecimalValue()}");
+        Fractions fraction3  = new Fractions("6","7");
+        Console.WriteLine($"The fraction form for object fraction3 is: {fraction3.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction3 is: {fraction3.GetDecimalValue()}");
+        Fractions fraction4 = new Fractions();
+        
+        fraction2.SetNumerator("5");
+        fraction3.SetNumerator("3");
+        fraction3.SetDenominator("4");
+        fraction4.SetNumerator("1");
+        fraction4.SetDenominator("3");
+
+        Console.WriteLine($"The fraction form for object fraction1 is: {fraction1.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction1 is: {fraction1.GetDecimalValue()}");
+        Console.WriteLine($"The fraction form for object fraction2 is: {fraction2.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction2 is: {fraction2.GetDecimalValue()}");
+        Console.WriteLine($"The fraction form for object fraction3 is: {fraction3.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction3 is: {fraction3.GetDecimalValue()}");
+        Console.WriteLine($"The fraction form for object fraction4 is: {fraction4.GetFractionString()}");
+        Console.WriteLine($"The decimal form for object fraction4 is: {fraction4.GetDecimalValue()}");
+
     }
 }
